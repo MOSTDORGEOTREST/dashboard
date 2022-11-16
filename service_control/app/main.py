51939d3f1793bfce1944d0300afa04ef73b626fb
tnick1502 <59287059+tnick1502@users.noreply.github.com>
2022-11-16 13:11:19 +0300

    Добавлен скрипт копирования бд

--- service_control/app/main.py
+++ service_control/app/main.py
@@ -1,8 +1,18 @@
 import uvicorn
+from threading import Thread
+
 from settings import settings
+from db_copy import copy_db
 from app import app
 
 if __name__ == "__main__":
+
+    parser_thread = Thread(
+        target=copy_db,
+        args=(),
+        daemon=True,
+    ).start()
+
     uvicorn.run(
         app,
         host=settings.server_host,
--- service_control/app/settings.py
+++ service_control/app/settings.py
@@ -6,18 +6,22 @@ config = conf[0]
 
 if config == 'server':
     db_path = "/databases/organization/"
+    db_copy_dir = "/files/Обмен/Никите Романовичу/db/"
 elif config == 'linux':
     db_path = "/home/tnick/databases/organization/"
+    db_copy_dir = "/run/user/1000/gvfs/smb-share:server=192.168.0.1,share=files//Обмен/Никите Романовичу/db/"
 elif config == 'mac':
     db_path = "/Users/mac1/Desktop/projects/databases/organization/"
+    db_copy_dir = "/Users/mac1/Desktop/projects/databases"
 
 
 class Settings(BaseSettings):
     server_host: str = "0.0.0.0"
     server_port: int = 8500
 
-    excel_staff: str = f'{db_path}staff.xlsx'
-    excel_work_types: str = f'{db_path}work_types.xlsx'
     database_url: str = f"sqlite:///{db_path}control.sqlite3"
+    database_file = f"{db_path}control.sqlite3"
+    db_copy_dir = db_copy_dir
+
 
 settings = Settings()
