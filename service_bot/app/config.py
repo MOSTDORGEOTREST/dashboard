4c0c7bae764eecd7066d57211c6e840f3da06845
tnick1502 <tnick1502@mail.ru>
2023-10-02 10:50:26 +0300

    fix: bot

--- service_bot/app/config.py
+++ service_bot/app/config.py
@@ -1,14 +1,10 @@
-from dotenv import load_dotenv
-from pydantic import Field
 import os
 
-load_dotenv(dotenv_path=os.path.normpath(".env"))
-
 class Config:
-    API_TOKEN: str = Field(..., env='API_TOKEN')
-    MDGT_CHAT_ID: str = Field(..., env='MDGT_CHAT_ID')
-    MDGT_CHANNEL_ID: str = Field(..., env='MDGT_CHANNEL_ID')
-    SERVER_URI: str = Field(..., env='SERVER_URI')
-    SERVER_CUSTOMER_URI: str = Field(..., env='SERVER_CUSTOMER_URI')
+    API_TOKEN: str = os.environ['API_TOKEN']
+    MDGT_CHAT_ID: str = os.environ['MDGT_CHAT_ID']
+    MDGT_CHANNEL_ID: str = os.environ['MDGT_CHANNEL_ID']
+    SERVER_URI: str = os.environ['SERVER_URI']
+    SERVER_CUSTOMER_URI: str = os.environ['SERVER_CUSTOMER_URI']
 
 configs = Config()
