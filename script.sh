e6438b1ea8d736c65df5de8a446da2149b46dbed
Nick <tnick1502@mail.ru>
2024-01-17 13:33:57 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,12 @@
-docker system prune -a -f
+docker system prune -af
 docker rm $(docker ps -a -q) -f
 docker rmi $(docker images -a -q) -f
+
+sudo rm -rf /var/log/*
+
 cd /home/user_bot/dashbord
 git pull
 sudo service docker restart
-docker-compose up
+docker-compose up -d
+
+docker-compose -f /home/user_bot/dockprom/docker-compose.yml up -d
